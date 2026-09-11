406db2757412b6c5c65298f28c30da04471f5be1
Sofia Valencia <sovalencia@unal.edu.co>
2026-09-11 16:45:08 -0500

    Pregunta01

--- PRE_01_hola_mundo/src/pregunta_01.py
+++ PRE_01_hola_mundo/src/pregunta_01.py
@@ -7,7 +7,7 @@ Escriba el codigo que ejecute la accion solicitada en cada pregunta.
 
 def pregunta_01():
     """
-    Retorne el string "Hola mundo cruel!"
+    Retorne el string "Hola mundo cruel!uu"
 
     """
 
